884b2187764c3ca287dea00db964faf1efe85a68
GuyueWK <545941709@qq.com>
2025-09-26 11:25:29 +0800

    change install script

---
+++ install_dependencies.sh
@@ -0,0 +1,2 @@
+sudo pacman -S --noconfirm zen-browser-bin fcitx5-im fcitx5-rime waybar kitty wofi swappy yazi wlogout swaync blueman wl-clipboard swww clapper cliphist
+paru -S --noconfirm ttf-iosevka-nerd zathura visual-studio-code-bin btop fastfetch cava
